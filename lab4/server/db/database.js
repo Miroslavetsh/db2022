f7863f0fad3d3eabdfd0c16c7a6d90dec5d6832a
Miroslavetsh <dfgshte@gmail.com>
2022-06-02 09:11:07 +0300

    Complete docker-compose settings, normalize for developmnet

--- lab4/server/db/database.js
+++ lab4/server/db/database.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
+const HOST = process.env.NODE_ENV === 'development' ? 'localhost' : 'data'
+
 module.exports = {
-  db: 'mongodb://data:27017/restapi',
+  db: `mongodb://${HOST}:27017/restapi`,
 }
